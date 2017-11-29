d8c0f9bc4d3041d8f461483d39833b3b0ffbc018
Trieulh-Gotit <trieu@gotitapp.co>
2017-11-29 17:50:35 +0700

    Generate Component with connect function - React-Redux

--- redux/chat_intermediate/src/App.js
+++ redux/chat_intermediate/src/App.js
@@ -1,134 +1,272 @@
 import React from 'react';
+import {createStore,combineReducers} from 'redux';
+import {Provider, connect} from 'react-redux';
+import uuid from 'uuid';
+// function createStore(reducer, initialState) {
+//   let state = initialState;
+//   const listeners = [];
 
-function createStore(reducer, initialState) {
-  let state = initialState;
-  const listeners = [];
+//   const subscribe = (listener) => (
+//     listeners.push(listener)
+//   );
 
-  const subscribe = (listener) => (
-    listeners.push(listener)
-  );
+//   const getState = () => (state);
 
-  const getState = () => (state);
+//   const dispatch = (action) => {
+//     state = reducer(state, action);
+//     listeners.forEach(l => l());
+//   };
 
-  const dispatch = (action) => {
-    state = reducer(state, action);
-    listeners.forEach(l => l());
-  };
+//   return {
+//     subscribe,
+//     getState,
+//     dispatch,
+//   };
+// }
 
-  return {
-    subscribe,
-    getState,
-    dispatch,
-  };
-}
+// function reducer(state = {}, action) {
+//   return {
+//     activeThreadId: activeThreadIdRecucer(state.activeThreadId, action),
+//     threads: threadsReducer(state.threads, action),
+//   }
+// }
 
-function reducer(state, action) {
-  if (action.type === 'ADD_MESSAGE') {
-    return {
-      messages: state.messages.concat(action.message),
-    };
-  } else if (action.type === 'DELETE_MESSAGE') {
-    return {
-      messages: [
-        ...state.messages.slice(0, action.index),
-        ...state.messages.slice(
-          action.index + 1, state.messages.length
-        ),
-      ],
-    };
+const reducer = combineReducers({
+  activeThreadId: activeThreadIdRecucer,
+  threads: threadsReducer,
+})
+
+function activeThreadIdRecucer(state = '1-fca2', action) {
+  if (action.type === 'OPEN_THREAD') {
+    return action.id
   } else {
     return state;
   }
 }
 
-const initialState = { messages: [] };
+function messagesReducer(state = [], action) {
+  switch (action.type) {
+    case 'ADD_MESSAGE':
+      {
+        const newMessage = {
+          text: action.text,
+          timestamp: Date.now(),
+          id: uuid.v4()
+        }
 
-const store = createStore(reducer, initialState);
-
-class App extends React.Component {
-  componentDidMount() {
-    store.subscribe(() => this.forceUpdate());
-  }
-
-  render() {
-    const messages = store.getState().messages;
-
-    return (
-      <div className='ui segment'>
-        <MessageView messages={messages} />
-        <MessageInput />
-      </div>
-    );
+        return state.concat(newMessage)
+      }
+    case 'DELETE_MESSAGE':
+      {
+        return state.filter((m) => (m.id !== action.id))
+      }
+    default:
+      {
+        return state
+      }
   }
 }
 
-class MessageInput extends React.Component {
+function threadsReducer(state = [{
+  id: '1-fca2',
+  title: 'Buzz Aldrin',
+  messages: messagesReducer(undefined, {})
+}, {
+  id: '2-be91',
+  title: 'Michael Collins',
+  messages: messagesReducer(undefined, {})
+}], action) {
+  switch (action.type) {
+    case 'ADD_MESSAGE':
+    case 'DELETE_MESSAGE':
+      {
+        const threadIndex = findThreadIndex(state, action)
+
+        const oldThread = state[threadIndex]
+
+        const newThread = {
+          ...oldThread,
+          messages: messagesReducer(oldThread.messages, action),
+          
+        }
+
+        return [
+          ...state.slice(0, threadIndex),
+          newThread,
+          ...state.slice(threadIndex + 1, state.length)
+        ]
+      }
+    default:
+      {
+        return state
+      }
+  }
+}
+
+function findThreadIndex(state, action) {
+  switch (action.type) {
+    case 'ADD_MESSAGE':
+      return state.findIndex((t) => t.id === action.threadId)
+    case 'DELETE_MESSAGE':
+      return state.findIndex((t) => t.messages.find((m) => (m.id === action.id)))
+    default:
+      return state
+  }
+}
+
+const initialState = {
+  activeThreadId: '1-fca2',
+  threads: [{ // Two threads in state
+      id: '1-fca2', // hardcoded pseudo-UUID 
+      title: 'Buzz Aldrin',
+      messages: [{ // This thread starts with a single message already 
+        text: 'Twelve minutes to ignition.',
+        timestamp: Date.now(),
+        id: uuid.v4()
+      }]
+    },
+    {
+      id: '2-be91',
+      title: 'Michael Collins',
+      messages: []
+    }
+  ]
+}
+
+const Tabs = (props) => (
+  <div className = 'ui top attached tabular menu' >
+    {
+      props.tabs.map((tab, index) => (
+        <div
+          key={index}
+          className={tab.active? 'active item': 'item'}
+          onClick={() => props.onClick(tab.id)}>
+          {tab.title}
+        </div>
+      ))
+    }
+  </div>
+)
+
+const mapStateToTabProps = (state) => {
+  const tabs = state
+    .threads
+    .map((t) => ({
+      title: t.title,
+      active: t.id === state.activeThreadId,
+      id: t.id
+    }))
+  
+    return {
+      tabs,
+    }
+}
+
+const mapDispatchtoTabProps = (dispatch) => ({
+  onClick: (id) => (store.dispatch({
+    type: 'OPEN_THREAD',
+    id: id
+  })),
+})
+
+
+const ThreadTabs = connect(
+  mapStateToTabProps,
+  mapDispatchtoTabProps
+)(Tabs)
+
+const Thread = (props) => (
+  <div className='ui center aligned basic segment'>
+    <MessageList messages={props.thread.messages} onClick={props.onMessageClick}/>
+    <TextFieldSubmit onSubmit={props.onMessageSubmit}/>
+  </div>
+)
+
+const mapStateToThreadProps = (state) => ({
+  thread: state.threads.find((t) => t.id === state.activeThreadId)
+})
+
+const mapDispatchtoThreadProps = (dispatch) => ({
+  onMessageClick: (id) => (
+    dispatch({
+      type: 'DELETE_MESSAGE',
+      id: id
+    })
+  ),
+  dispatch: dispatch,
+})
+
+const mergeThreadProps = (stateProps, dispatchProps) => ({
+  ...stateProps,
+  ...dispatchProps,
+  onMessageSubmit: (text) => (
+    dispatchProps.dispatch({
+      type: 'ADD_MESSAGE',
+      text: text,
+      threadId: stateProps.thread.id,
+    })
+  )
+})
+
+const ThreadDisplay = connect(
+  mapStateToThreadProps,
+  mapDispatchtoThreadProps,
+  mergeThreadProps
+)(Thread)
+
+const MessageList = (props) => (
+  <div className = 'ui comments' >
+    {
+      props.messages.map((message, index) => (
+        <div className='comment' key={index} onClick={() => props.onClick(message.id)}>
+          {message.text}
+          <span className='metadata'>@{message.timestamp}
+          </span>
+        </div>
+      ))
+    }
+  </div>
+)
+
+class TextFieldSubmit extends React.Component {
   state = {
-    value: '',
+    value: ''
   };
 
   onChange = (e) => {
-    this.setState({
-      value: e.target.value,
-    })
+    this.setState({value: e.target.value})
   };
 
   handleSubmit = () => {
-    store.dispatch({
-      type: 'ADD_MESSAGE',
-      message: this.state.value,
-    });
-    this.setState({
-      value: '',
-    });
+    this.props.onSubmit(this.state.value)
+    this.setState({value: ''});
   };
 
   render() {
     return (
       <div className='ui input'>
-        <input
-          onChange={this.onChange}
-          value={this.state.value}
-          type='text'
-        />
-        <button
-          onClick={this.handleSubmit}
-          className='ui primary button'
-          type='submit'
-        >
+        <input onChange={this.onChange} value={this.state.value} type='text'/>
+        <button onClick={this.handleSubmit} className='ui primary button' type='submit'>
           Submit
         </button>
-       </div>
-    );
-  }
-}
-
-class MessageView extends React.Component {
-  handleClick = (index) => {
-    store.dispatch({
-      type: 'DELETE_MESSAGE',
-      index: index,
-    });
-  };
-
-  render() {
-    const messages = this.props.messages.map((message, index) => (
-      <div
-        className='comment'
-        key={index}
-        onClick={() => this.handleClick(index)}
-      >
-        {message}
-      </div>
-    ));
-    return (
-      <div className='ui center aligned basic segment'>
-        <div className='ui comments'>
-          {messages}
-        </div>
       </div>
     );
   }
 }
 
-export default App;
+const store = createStore(reducer, initialState);
+
+const App = () => (
+  <div className='ui segment'>
+    <ThreadTabs/>
+    <ThreadDisplay/>
+  </div>
+)
+
+const WrappedApp = () => (
+  <Provider store={store}>
+    <App/>
+  </Provider>
+)
+
+export default WrappedApp
